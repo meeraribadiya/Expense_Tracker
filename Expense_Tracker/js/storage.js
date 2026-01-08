7d1407b227ec7e81db278191afacaeda5f712dfc
Mira Ribadiya <miraribadiya45@gmail.com>
2026-01-08 09:05:02 +0530

    Delete Expense_Tracker directory

--- Expense_Tracker/js/storage.js
+++
@@ -1,7 +0,0 @@
-function getData() {
-    return JSON.parse(localStorage.getItem("transactions")) || [];
-}
-
-function saveData(arr) {
-    localStorage.setItem("transactions", JSON.stringify(arr));
-}
